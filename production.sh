fabb5753db6851206ef0b098c64ba0a897be40db
谭林华 <linhua.tlh@alibaba-inc.com>
2016-07-05 20:39:52 +0800

    init build.sh

--- production.sh
+++ production.sh
@@ -6,6 +6,8 @@ VERSION=0.5
 docker build -t registry.aliyuncs.com/acs-access/proxy:$VERSION .
 docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION
 docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
+
+docker push registry.aliyuncs.com/acs-access/proxy:$VERSION
 docker push registry.aliyuncs.com/acs/proxy:$VERSION
 docker push registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
 
