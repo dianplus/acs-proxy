88a7587c317b282912e91425f15a2ec9b4d019cc
谭林华 <linhua.tlh@alibaba-inc.com>
2016-07-05 21:21:49 +0800

    init build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,15 @@ set -e
 
 GIT_SHA=`git rev-parse --short HEAD || echo "HEAD"`
 VERSION=0.5
+IMAGE_VERSION=$VERSION-$GIT_SHA
+
+function cleanup() {
+	mv acshaproxy/config.py.bak acshaproxy/config.py
+}
+
+trap cleanup  EXIT
+sed -i.bak "s/HEAD/$IMAGE_VERSION/g" ./acshaproxy/config.py
+
 docker build -t registry.aliyuncs.com/acs-access/proxy:$VERSION .
 docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs-access/proxy:$VERSION-$GIT_SHA 
 docker push registry.aliyuncs.com/acs-access/proxy:$VERSION
--- production.sh
+++ production.sh
@@ -3,6 +3,16 @@
 set -e
 GIT_SHA=`git rev-parse --short HEAD || echo "HEAD"`
 VERSION=0.5
+export IMAGE_VERSION=$VERSION-$GIT_SHA
+
+
+function cleanup() {
+        mv acshaproxy/config.py.bak acshaproxy/config.py
+}
+
+trap cleanup  EXIT
+sed -i.bak "s/HEAD/$IMAGE_VERSION/g" ./acshaproxy/config.py
+
 docker build -t registry.aliyuncs.com/acs-access/proxy:$VERSION .
 docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION
 docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
