b58a4842d1afc8604a6ef748b4cd455ebbf3e330
谭林华 <linhua.tlh@alibaba-inc.com>
2017-01-03 14:39:56 +0800

    rebuild

--- production.sh
+++ production.sh
@@ -13,11 +13,11 @@ function cleanup() {
 trap cleanup  EXIT
 sed -i.bak "s/HEAD/$IMAGE_VERSION/g" ./acshaproxy/config.py
 
-docker build -t registry.aliyuncs.com/acs-access/proxy:$VERSION .
-docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION
-docker tag registry.aliyuncs.com/acs-access/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
+docker build -t registry.aliyuncs.com/acs-sample/proxy:$VERSION .
+docker tag registry.aliyuncs.com/acs-sample/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION
+docker tag registry.aliyuncs.com/acs-sample/proxy:$VERSION registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
 
-docker push registry.aliyuncs.com/acs-access/proxy:$VERSION
+docker push registry.aliyuncs.com/acs-sample/proxy:$VERSION
 docker push registry.aliyuncs.com/acs/proxy:$VERSION
 docker push registry.aliyuncs.com/acs/proxy:$VERSION-$GIT_SHA
 
