a1976426f0cb786107353d6a1d582ce4b757bb97
quanzhao.cqz <quanzhao.cqz@alibaba-inc.com>
2017-09-07 13:44:16 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -7,14 +7,15 @@ VERSION=`cat acshaproxy/__init__.py | awk -F"\"" '{ print $2 }'`
 IMAGE_VERSION=$VERSION-$GIT_SHA
 
 function cleanup() {
-	mv acshaproxy/config.py.bak acshaproxy/config.py
+    mv acshaproxy/config.py.bak acshaproxy/config.py
 }
 
 trap cleanup  EXIT
 sed -i.bak "s/HEAD/$IMAGE_VERSION/g" ./acshaproxy/config.py
 
-docker build -t registry.aliyuncs.com/acs-sample/proxy:$VERSION .
-docker tag registry.aliyuncs.com/acs-sample/proxy:$VERSION registry.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA 
-docker push registry.aliyuncs.com/acs-sample/proxy:$VERSION
-docker push registry.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
+docker build -t registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION .
+docker tag registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION registry.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
+
+#docker push registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION
+#docker push registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
 
