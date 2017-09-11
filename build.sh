e323791aaf03b2bf0b8fc0472cd22cd85546c7f0
quanzhao.cqz <quanzhao.cqz@alibaba-inc.com>
2017-09-11 10:07:50 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,19 +3,18 @@
 set -e
 
 GIT_SHA=`git rev-parse --short HEAD || echo "HEAD"`
-VERSION=`cat acshaproxy/__init__.py | awk -F"\"" '{ print $2 }'`
+VERSION=`cat ./acshaproxy/__init__.py | awk -F"\"" '{ print $2 }'`
 IMAGE_VERSION=$VERSION-$GIT_SHA
 
 function cleanup() {
-    mv acshaproxy/config.py.bak acshaproxy/config.py
+	mv acshaproxy/config.py.bak acshaproxy/config.py
 }
 
 trap cleanup  EXIT
 sed -i.bak "s/HEAD/$IMAGE_VERSION/g" ./acshaproxy/config.py
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION .
-docker tag registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
-
-#docker push registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION
-#docker push registry.cn-hangzhou.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
+docker build -t registry.aliyuncs.com/acs-sample/proxy:$VERSION .
+docker tag registry.aliyuncs.com/acs-sample/proxy:$VERSION registry.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA 
+docker push registry.aliyuncs.com/acs-sample/proxy:$VERSION
+docker push registry.aliyuncs.com/acs-sample/proxy:$VERSION-$GIT_SHA
 
